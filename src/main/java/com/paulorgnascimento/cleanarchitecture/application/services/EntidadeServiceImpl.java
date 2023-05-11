b5e3b5898daae97a8831632409c72955c6151858
paulorgnascimento <124225900+paulorgnascimento@users.noreply.github.com>
2023-05-11 00:51:30 -0300

    Controllers e Services

---
+++ src/main/java/com/paulorgnascimento/cleanarchitecture/application/services/EntidadeServiceImpl.java
@@ -0,0 +1,13 @@
+package com.paulorgnascimento.cleanarchitecture.application.services;
+
+import com.paulorgnascimento.cleanarchitecture.application.dto.EntidadeDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EntidadeServiceImpl implements EntidadeService {
+
+    @Override
+    public void criarEntidade(EntidadeDto entidadeDto) {
+
+    }
+}
