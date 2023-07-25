4111d4f81339c944a82f7b7af3056addb5b8eae6
paulorgnascimento <124225900+paulorgnascimento@users.noreply.github.com>
2023-07-25 00:26:51 -0300

    Refactory

--- src/main/java/com/paulorgnascimento/cleanarchitecture/application/mapper/EntidadeMapper.java
+++ src/main/java/com/paulorgnascimento/cleanarchitecture/application/mapper/EntidadeMapper.java
@@ -2,9 +2,11 @@ package com.paulorgnascimento.cleanarchitecture.application.mapper;
 
 import com.paulorgnascimento.cleanarchitecture.application.dto.EntidadeDto;
 import com.paulorgnascimento.cleanarchitecture.domain.entity.Entidade;
+import com.paulorgnascimento.cleanarchitecture.infrastructure.persistence.entity.EntidadeMapping;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface EntidadeMapper {
-    Entidade toEntity(EntidadeDto dto);
+    Entidade fromDto(EntidadeDto dto);
+    EntidadeMapping fromEntity(Entidade entity);
 }
--- src/main/java/com/paulorgnascimento/cleanarchitecture/application/services/EntidadeServiceImpl.java
+++ src/main/java/com/paulorgnascimento/cleanarchitecture/application/services/EntidadeServiceImpl.java
@@ -2,10 +2,9 @@ package com.paulorgnascimento.cleanarchitecture.application.services;
 
 import com.paulorgnascimento.cleanarchitecture.application.dto.EntidadeDto;
 import com.paulorgnascimento.cleanarchitecture.application.mapper.EntidadeMapper;
-import com.paulorgnascimento.cleanarchitecture.application.mapper.TB_EntidadeMapper;
 import com.paulorgnascimento.cleanarchitecture.domain.entity.Entidade;
 import com.paulorgnascimento.cleanarchitecture.infrastructure.gateway.Todo;
-import com.paulorgnascimento.cleanarchitecture.infrastructure.persistence.entity.TB_Entidade;
+import com.paulorgnascimento.cleanarchitecture.infrastructure.persistence.entity.EntidadeMapping;
 import com.paulorgnascimento.cleanarchitecture.infrastructure.persistence.repository.EntidadeRepository;
 import org.springframework.stereotype.Service;
 
@@ -13,16 +12,12 @@ import org.springframework.stereotype.Service;
 public class EntidadeServiceImpl implements EntidadeService {
 
     private final EntidadeMapper entidadeMapper;
-    private final TB_EntidadeMapper entidadeTableDtoMapper;
     private final EntidadeRepository entidadeTableDtoRepository;
-
     private final GetTodoService getTodoService;
 
     public EntidadeServiceImpl(EntidadeMapper entidadeMapper,
-                               TB_EntidadeMapper entidadeTableDtoMapper,
                                EntidadeRepository entidadeRepository, GetTodoService getTodoService) {
         this.entidadeMapper = entidadeMapper;
-        this.entidadeTableDtoMapper = entidadeTableDtoMapper;
         this.entidadeTableDtoRepository = entidadeRepository;
         this.getTodoService = getTodoService;
     }
@@ -32,9 +27,9 @@ public class EntidadeServiceImpl implements EntidadeService {
 
         Todo todo = getTodoService.execute(1);
 
-        Entidade entidade = entidadeMapper.toEntity(entidadeDto);
-        TB_Entidade TBEntidade = entidadeTableDtoMapper.toTableDto(entidade);
+        Entidade entidade = entidadeMapper.fromDto(entidadeDto);
+        EntidadeMapping entidadeMapping = entidadeMapper.fromEntity(entidade);
 
-        entidadeTableDtoRepository.save(TBEntidade);
+        entidadeTableDtoRepository.save(entidadeMapping);
     }
 }
